f6f5e3e7d38702ba149e2b623c15db647ebd8414
bhaskarshankar488 <bhaskarshankar488@gmail.com>
2026-06-23 12:18:27 +0530

    we add feature is alternative select in in tool content

--- src/features/ToolAlternative/content/alternative.initial.ts
+++ src/features/ToolAlternative/content/alternative.initial.ts
@@ -2,9 +2,14 @@ import type { AlternativeFormData } from "../types/alternative.types";
 
 export const initialAlternative: AlternativeFormData =
 {
+
+   toolId: "",
   title: "",
   slug: "",
   pageDescription: "",
+
+  mainToolName: "",
+  mainToolBrand: "",
 
   seo: {
     metaTitle: "",
